711a6698963f629db22e6f69dffce5a1efa531ba
user-promi <promita@dualcube.com>
2025-07-29 17:50:13 +0530

    Fixed recaptcha related issue

--- classes/class-mvx-user.php
+++ classes/class-mvx-user.php
@@ -156,10 +156,12 @@ class MVX_User {
                     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
                     $recaptcha_response = isset($_POST['recaptchav3Response']) ? $_POST['recaptchav3Response'] : '';
 
-                    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
-                    $recaptcha = json_decode($recaptcha);
-
-                    if (!$recaptcha->success || $recaptcha->score < 0.5) {
+                    $response = wp_remote_get($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+                    $recaptcha = json_decode($response['body']);
+                    
+                    if ($recaptcha->success && $recaptcha->score >= 0.5) {
+                        return $recaptcha->score;
+                    } else {
                         $validation_errors->add('recaptcha is not validate', __('Please Verify  Recaptcha', 'multivendorx'));
                     }
                 }
