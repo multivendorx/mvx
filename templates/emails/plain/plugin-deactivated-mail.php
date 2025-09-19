27fee0c032990eea0a1dea103ff596587f92e8b9
DAnn2012 <dannunziata@hotmail.it>
2025-09-19 11:25:53 +0200

    Update plugin-deactivated-mail.php

--- templates/emails/plain/plugin-deactivated-mail.php
+++ templates/emails/plain/plugin-deactivated-mail.php
@@ -18,13 +18,13 @@ global $MVX;
 echo "= " . $email_heading . " =\n\n";
 echo sprintf( __("Hi %s, We are sorry to see that you have deactivated MultiVendorX.", 'multivendorx' ), $user_login );
 echo '\n';
-echo _e('As we always look out for our users, even though they are not using our plugin, can you tell us why you deactivate them?', 'multivendorx');
+_e('As we always look out for our users, even though they are not using our plugin, can you tell us why you deactivate them?', 'multivendorx');
 echo '\n';
-echo _e('Maybe we can help set up the plugin or assist you in any way possible.', 'multivendorx');
+_e('Maybe we can help set up the plugin or assist you in any way possible.', 'multivendorx');
 echo '\n';
-echo _e('Hope to hear from you soon.', 'multivendorx');
+_e('Hope to hear from you soon.', 'multivendorx');
 echo '\n';
-echo _e('Have a great day.', 'multivendorx');
+_e('Have a great day.', 'multivendorx');
 
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 echo apply_filters( 'mvx_email_footer_text', get_option( 'mvx_email_footer_text' ) );
